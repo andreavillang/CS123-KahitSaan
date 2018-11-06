eafee9be74bdea03912a8a0b950f6ad6526c1c3c
Andrea Ang <andreavillang@gmail.com>
2018-11-06 22:02:59 +0800

    Added specific search auto-complete function

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(function() {
+    var alreadyFilled = false;
+    var restaurants = ['Bon Chon', 'Kebab Express', 'Kenny Rodgers', 'KFC', 'McDonalds'];
+
+    function initDialog() {
+        clearDialog();
+        for (var i = 0; i < restaurants.length; i++) {
+            $('.dialog').append('<div>' + restaurants[i] + '</div>');
+        }
+    }
+    function clearDialog() {
+        $('.dialog').empty();
+    }
+    $('.autocomplete input').click(function() {
+        if (!alreadyFilled) {
+            $('.dialog').addClass('open');
+        }
+
+    });
+    $('body').on('click', '.dialog > div', function() {
+        $('.autocomplete input').val($(this).text()).focus();
+        $('.autocomplete .close').addClass('visible');
+        alreadyFilled = true;
+    });
+    $('.autocomplete .close').click(function() {
+        alreadyFilled = false;
+        $('.dialog').addClass('open');
+        $('.autocomplete input').val('').focus();
+        $(this).removeClass('visible');
+    });
+
+    function match(str) {
+        str = str.toLowerCase();
+        clearDialog();
+        for (var i = 0; i < restaurants.length; i++) {
+            if (restaurants[i].toLowerCase().startsWith(str)) {
+                $('.dialog').append('<div>' + restaurants[i] + '</div>');
+            }
+        }
+    }
+    $('.autocomplete input').on('input', function() {
+        $('.dialog').addClass('open');
+        alreadyFilled = false;
+        match($(this).val());
+    });
+    $('body').click(function(e) {
+        if (!$(e.target).is("input, .close")) {
+            $('.dialog').removeClass('open');
+        }
+    });
+    initDialog();
+});
